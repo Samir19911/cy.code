e86993eae45040a38d1bc502d5aed5a053bb95cd
Бабаев Самир <samir1605@mail.ru>
2025-03-02 14:10:32 +0300

    Add files via upload

---
+++ cypress/e2e/autotest.cy.js
@@ -0,0 +1,69 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный пароль и верный логин', function () {
+         cy.visit('https://login.qa.studio/');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+     
+     
+     it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Неверный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Верный пароль и неверный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('gman@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Проверка валидации. Ввод логина без @', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Строчные буквы', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    
+})
+
+ 
+ 
+ // запуск через теринал: npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome
+ 
